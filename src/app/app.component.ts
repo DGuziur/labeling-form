edecddbb00efe03f35f4194b46403e2f2544241b
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-09-24 11:38:29 +0200

    geting grasp on three.js (ng wrapper)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LabelThreemodelComponent } from './label-threemodel/label-threemodel.component';
+import { NgtCanvas } from 'angular-three';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LabelThreemodelComponent, NgtCanvas],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'labeling-form';
+  readonly model = LabelThreemodelComponent;
 }
---
+++ src/app/label-threemodel/label-threemodel.component.ts
@@ -0,0 +1,34 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject } from '@angular/core';
+import { extend } from 'angular-three';
+import { NgtCanvas, NgtArgs } from 'angular-three';
+import { NGT_STORE } from 'angular-three';
+import { OrbitControls } from 'three-stdlib';
+import * as THREE from 'three';
+
+extend(THREE);
+extend({ OrbitControls });
+
+@Component({
+  selector: 'label-model',
+  standalone: true,
+  imports: [NgtCanvas, NgtArgs],
+  templateUrl: './label-threemodel.component.html',
+  styleUrl: './label-threemodel.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class LabelThreemodelComponent {
+  private readonly store = inject(NGT_STORE);
+  readonly camera = this.store.get('camera');
+  readonly glDom = this.store.get('gl', 'domElement');
+
+  config = {
+    rowElementsCount: 3,
+    rowCount: 3,
+    rowHeight: 3,
+    columnCount: 6,
+    columnWidth: 1,
+    margin: 0.1,
+  };
+
+  readonly pi = Math.PI;
+}
